cec5f4c13dbeb94d5dfe80d0b6cfbf8ea6ee99ac
hqtang33 <hqtang33@gmail.com>
2018-03-19 11:27:18 +0800

    Added Card class

---
+++ src/Card.java
@@ -0,0 +1,18 @@
+
+public class Card {
+	private String color;
+	private String type;
+	
+	public Card(String color, String type) {
+		this.color = color;
+		this.type = type;
+	}
+	
+	public String getColor() {
+		return this.color;
+	}
+	
+	public String getType() {
+		return this.type;
+	}
+}
